73e3dd791231e3acf6a5a98575a41896421052aa
amippy <aminaka1223@gmail.com>
2015-12-26 16:36:10 +0900

    create message model

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,4 +1,16 @@
 class MessagesController < ApplicationController
   def index
+    @messages=Message.all
+  end
+
+  def create
+    @messages = Message.new(message_params)
+    @messages.save
+    redirect_to root_path, notice: 'メッセージを保存しました！'
+  end
+
+  private
+  def message_params
+    params.require(:message).permit(:name,:body)
   end
 end
